2d478c8d20955969a2546464ab9eac4e210a8f6e
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-04-21 11:05:27 +0100

    side-nav done/image-grid half way done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+function openNav(){
+    document.getElementById("side-nav").style.width = "250px";
+  }
+  
+  function closeNav(){
+    document.getElementById("side-nav").style.width = "0"; 
+  }
