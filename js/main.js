9e5a9c06cfe67244d84db5dd159a0f2f7dcc1cb3
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-04-29 17:29:16 +0100

    29/4/2019

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
 
-function openNav(){
+  function openNav(){
     document.getElementById("side-nav").style.width = "250px";
-  }
+    document.getElementById("menubutton").style.color = "transparent";
+    }
   
-  function closeNav(){
+    function closeNav(){
     document.getElementById("side-nav").style.width = "0"; 
-  }
+    document.getElementById("menubutton").style.color = "black";
+    }
+    function growButton(){
+      document.getElementById("menubutton").style.font.size = "50px";
+    }
