67cbed1258353061c43f2c7e2b363322e5f7ae6c
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-05-13 22:42:03 +0100

    aula 13-05

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 
 
-  function openNav(){
-    document.getElementById("side-nav").style.width = "250px";
-    document.getElementById("menubutton").style.color = "transparent";
+    function openNav(){
+      document.getElementById("side-nav").style.width = "250px";
+      document.getElementById("menubutton").style.color = "transparent";
     }
   
     function closeNav(){
-    document.getElementById("side-nav").style.width = "0"; 
-    document.getElementById("menubutton").style.color = "black";
+      document.getElementById("side-nav").style.width = "0"; 
+      document.getElementById("menubutton").style.color = "black";
     }
     function growButton(){
       document.getElementById("menubutton").style.font.size = "50px";
     }
 
 
-    $(document).ready(function(){
-      var l = $('.img-slider .img-slide .list').length;
-      var w = $('.img-slider .img-slide .list').innerWidth();
-      var final = l*w;
-      $('.img-slider .img-slide').css('width', final);
-    })
+   
