a848958be8dae9675ac8c273409bcd3285e3b4e2
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-05-27 17:08:46 +0100

    27-5

--- js/main.js
+++ js/main.js
@@ -2,18 +2,27 @@
     /*open and close side navbar*/
     function openNav(){
       document.getElementById("side-nav").style.width = "250px";
-      document.getElementById("menubutton").style.color = "transparent";
+      document.getElementById("menubutton").style.display = "none";
     }
   
     function closeNav(){
       document.getElementById("side-nav").style.width = "0"; 
-      document.getElementById("menubutton").style.color = "black";
+      document.getElementById("menubutton").style.display = "block";
     }
     function growButton(){
       document.getElementById("menubutton").style.font.size = "50px";
     }
 
     
+
+
+
+
+
+
+
+
+
     /*function for dropdown*/
     function showDrop(){
       document.getElementById("dropdown-list").style.display.show;
