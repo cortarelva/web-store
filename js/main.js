9c30db236aa3440a5f7263087c3313d04cf6316d
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-06-15 00:00:12 +0100

    14-06-2019

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
+    $(document).ready(function(){
+      $('#side-nav').click(function(){
+      $(this).css('width','250px');
+      });
+  });
+    
+    
     /*open and close side navbar*/
     function openNav(){
       document.getElementById("side-nav").style.width = "250px";
@@ -9,17 +16,14 @@
       document.getElementById("side-nav").style.width = "0"; 
       document.getElementById("menubutton").style.display = "block";
     }
+    
     function growButton(){
       document.getElementById("menubutton").style.font.size = "50px";
     }
 
-    
-   $(document).ready(function(){
-     var l = $('.img-slider .img-slide .pic').length;
-     var w = $('.img-slider .img-slide .pic').innerWidth();
-     var final = l*w;
-     $('.img-slider .img-slide').css('width', final);
-   })
+
+
+
    
 
 
