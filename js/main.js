16d2251c66afe0dd8c044ed41a1e5cbf3302d350
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-06-24 17:04:37 +0100

    24-06-2019 navbar jquery 50% done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 
-   /* $(document).ready(function(){
-      $('#side-nav').click(function(){
-      $(this).css('width','250px');
-      });
-  });*/
+     $(document).ready(function(){
+      var counter = 0;
+       var x = $('#side-nav');
+       var disp = $('#menubutton');
+       
+       $('#menubutton').click(function(){
+         x.css('width', '250px');
+         disp.css('display','none');
+         counter = 1;
+         
+       
+       if(counter == 1){
+         $('a').click(function(){
+           x.css('width', '0px');
+           disp.css('display','block');
+           counter = 0 ;
+           });     
+          }
+       });
+     })
     
+
+
+
+
+
+
     
     /*open and close side navbar*/
-    function openNav(){
+    /*function openNav(){
       document.getElementById("side-nav").style.width = "250px";
       document.getElementById("menubutton").style.display = "none";
     }
@@ -15,7 +36,7 @@
     function closeNav(){
       document.getElementById("side-nav").style.width = "0"; 
       document.getElementById("menubutton").style.display = "block";
-    }
+    }*/
     
     
 
