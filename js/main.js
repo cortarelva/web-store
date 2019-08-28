a5717c9d064f2fe5327ca810243755b087a6f40e
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-08-29 00:13:16 +0100

    shipping form validator done

--- js/main.js
+++ js/main.js
@@ -11,36 +11,32 @@
          disp.css('display','none');
          counter = 1;
          
-       
        if(counter == 1){
          $('a').click(function(){
            nav.css('width', '0px');
            disp.css('display','block');
            counter = 0 ;
-           });     
-          }
+          });     
+        }
 
           nav.on('mouseleave',function(){
             nav.css('width','0px');
             disp.css('display','block');
             counter = 0;
           });
-       });
-     });
+        });
+      });
     
 
 
-     //Function to validate email
+     //Function to validate newsletter email submission
 
-     $('#emailInput').click(function(e){
-      e.preventDefault();
-     });
-
-     
-     $(function(){
-	     
+  $('#emailInput').click(function(e){
+    e.preventDefault();
+  });
+ 
+    $(function(){ 
       $('#email-form').on('submit', () =>{
-        
         let hasError = false;
         let emailaddressVal = $('#emailInput').val();
         const emailReg = /^[\w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
@@ -63,14 +59,14 @@
          
         if(hasError == true){ 
           return false; }	
-      });
+        });
 
       //Close the modal and clear input field
       $(".email-modal-close").on("click", () => {
 	      $(".email-modal").css("display", "none");
 	      $('#emailInput').val('');
+      });
     });
-   });
 
    
    
@@ -81,15 +77,15 @@
 //function for slick slider    
  $(function(){
   $('.slider-container').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    responsive: [{
+     dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      arrows: true,
+      responsive: [{
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
@@ -97,11 +93,11 @@
       }
     },
     {
-       breakpoint: 400,
-       settings: {
-          arrows: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
+      breakpoint: 400,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
         }
       }]
     });
@@ -136,5 +132,46 @@ $(function(){
 });
 
 
- //function to enable lookbook popup
+ //function to enable lightbox
  
+
+
+
+
+//shipping form validator
+$(function(){
+	$("form[name = 'shippinginfo']").validate({
+		rules:{
+			shippingmethod:"required",
+			firstname:"required",
+			lastname:"required",
+			addressline1:"required",
+			city:"required",
+			postalcode:"required",
+			phone:{required: true, minlength: 9},
+			email:{required: true, email: true}
+		},
+		messages:{
+      shippingmethod:"Please select a delivery method",
+			firstname:"Please enter your first name",
+			lastname:"Please enter your last name",
+			adressline1:"Please enter your address",
+			city:"Please enter your city",
+      postalcode:"Please enter your postal code",
+      email:"Please enter a valid email address",
+			phone:{
+				required:"Please provide a phone number",
+				minlength:"Number must be at least 9 digits long"
+			}
+    },
+    
+		 submitHandler: function(form){
+			form.submit();
+		}
+	});
+})
+// function to submit form
+		const submitForm = () => {
+      document.form["shippinginfo"].submit();
+      return true;		
+	  } 
